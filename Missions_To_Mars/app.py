8d95e9f64ec52e7992a3e0b2289c78465ce93a72
Robert Gonzalez <robertsgonzalez92@yahoo.com>
2022-06-12 23:21:35 -0500

    flask progress

--- Missions_To_Mars/app.py
+++ Missions_To_Mars/app.py
@@ -1,12 +1,33 @@
 from flask import Flask, render_template, redirect
 from flask_pymongo import PyMongo
-import scrape_mars
+from pymongo import DESCENDING
+import scrape_mars  # from scrape_mars import *
 
 # Create an instance of Flask
 app = Flask(__name__)
 
 # Use PyMongo to establish Mongo connection
-mongo = PyMongo(app, uri="mongodb://localhost:27017/weather_app")
+mongo = PyMongo(app, uri="mongodb://localhost:27017/mars")
+
+
+
+# Route that will trigger the scrape function
+@app.route("/scrape")
+def scrape():
+
+    # scrape mars data
+    mars_data = {
+        "image": scrape_mars.scrape_scan_image(),
+        "title": scrape_mars.scrape_mars_title(),
+        "paragraph": scrape_mars.scrape_mars_para()
+    }
+
+    # Insert mars data into mongodb
+    collection = mongo.db["scrapedata"]
+    collection.insert_one(mars_data)
+
+    # Redirect back to home page
+    return redirect("/")
 
 
 # Route to render index.html template using data from Mongo
@@ -14,24 +35,23 @@ mongo = PyMongo(app, uri="mongodb://localhost:27017/weather_app")
 def home():
 
     # Find one record of data from the mongo database
-    destination_data = mongo.db.collection.find_one()
+    collection = mongo.db["scrapedata"]
+    mars_data = collection.find_one(sort=[('_id', DESCENDING)])
+
+    # if no data in mongo, scrape/insert some into mongo
+    if mars_data==None:
+        return redirect("/scrape")
 
     # Return template and data
-    return render_template("index.html", vacation=destination_data)
+    return render_template("index.html",
+        image_link = mars_data["image"],
+        mars_title = mars_data["title"],
+        mars_para = mars_data["paragraph"]
+    )
 
 
-# Route that will trigger the scrape function
-@app.route("/scrape")
-def scrape():
 
-    # Run the scrape function
-    mars_data = scrape_mars.scrape()
 
-    # Insert the record
-    mongo.db.collection.update_one({}, {"$set": mars_data}, upsert=True)
-
-    # Redirect back to home page
-    return redirect("/")
 
 
 if __name__ == "__main__":
--- Missions_To_Mars/scrape_mars.py
+++ Missions_To_Mars/scrape_mars.py
@@ -3,46 +3,30 @@ from bs4 import BeautifulSoup as bs
 import time
 from webdriver_manager.chrome import ChromeDriverManager
 
+executable_path = {'executable_path': ChromeDriverManager().install()}
+browser = Browser('chrome', **executable_path, headless=True)
 
-def scrape():
-    # Set up Splinter
-    executable_path = {'executable_path': ChromeDriverManager().install()}
-    browser = Browser('chrome', **executable_path, headless=False)
 
-    # Visit visitcostarica.herokuapp.com
-    url = "https://spaceimages-mars.com/"
-    browser.visit(url)
-
+def scrape_scan_image():
+    browser.visit("https://spaceimages-mars.com/")
     browser.find_by_tag('button')[1].click()
+    soup = bs(browser.html, 'html.parser')
+    soup.find('img', class_='fancybox-image').get('src')
+    img_url_rel = soup.find('img', class_='fancybox-image').get('src')
+    img_url_abs = f'https://spaceimages-mars.com/{img_url_rel}'
+    return img_url_abs
 
-    # time.sleep(1)
+def scrape_mars_title():
+    browser.visit("https://redplanetscience.com/")
+    soup = bs(browser.html, 'html.parser')
+    result = soup.find('div', class_='content_title')
+    title = result.text
+    return title
 
-    # Scrape page into Soup
-    html = browser.html
-    soup = bs(html, "html.parser")
+def scrape_mars_para():
+    browser.visit("https://redplanetscience.com/")
+    soup = bs(browser.html, 'html.parser')
+    result = soup.find('div', class_='article_teaser_body')
+    para = result.text
+    return para
 
-    # Get the average temps
-    avg_temps = soup.find('div', id='weather')
-
-    # Get the min avg temp
-    min_temp = avg_temps.find_all('strong')[0].text
-
-    # Get the max avg temp
-    max_temp = avg_temps.find_all('strong')[1].text
-
-    # BONUS: Find the src for the sloth image
-    relative_image_path = soup.find_all('img')[2]["src"]
-    sloth_img = url + relative_image_path
-
-    # Store data in a dictionary
-    mars_data = {
-        "sloth_img": sloth_img,
-        "min_temp": min_temp,
-        "max_temp": max_temp
-    }
-
-    # Close the browser after scraping
-    browser.quit()
-
-    # Return results
-    return mars_data
